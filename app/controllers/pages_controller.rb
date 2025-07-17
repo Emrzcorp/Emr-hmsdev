bffda5a2887dfd7396510e632cfc03d8ae990763
Ankit <ankitkusumakar55954@gmail.com>
2025-07-17 19:23:07 +0530

    fixed log in page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,94 @@
+class PagesController < ApplicationController
+  before_action :authenticate_user!
+  def home
+  end
+
+  def ot
+  end
+
+  def appointment
+  end
+  
+  def Confirmed
+  end
+   
+  def collection
+  end 
+
+  def requests
+  end 
+
+  def settings
+  end 
+
+  def day
+  end 
+
+  def week
+  end 
+
+  def ehrsetting
+  end 
+
+  def intake
+  end
+
+  def practicefusiondefaultform
+  end
+
+  def reviewquestion
+  end
+
+  def task
+  end
+
+  def mytask
+  end
+
+  def tool
+  end
+  
+  def unassignedtask
+  end
+
+  def rxchange
+  end 
+
+  def labresults
+  end
+
+  def refillrequests
+  end
+
+  def imagingresults
+  end
+
+  def chart
+  end
+  
+  def scheduled
+  end
+
+  def recentpatients
+  end
+
+  def patientrecoad
+  end
+
+  def timeline
+  end
+
+  def profile
+  end
+
+  def recoaddate
+  end
+
+  def displaysetting
+  end
+
+  def appointment_modal
+    render partial: 'pages/appointment' # This will use app/views/pages/_appointment.html.erb
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,39 @@
 Rails.application.routes.draw do
+  root "pages#ot"
+  get 'pages/home'
+  devise_for :users
   get 'home/index'
+  get "ot", to: "pages#ot"
+  get "appointment", to: "pages#appointment"
+  get "Confirmed", to: "pages#Confirmed"
+  get "collection", to: "pages#collection" 
+  get "requests", to: "pages#requests" 
+  get "settings", to: "pages#settings" 
+  get "ehrsetting", to: "pages#ehrsetting" 
+  get "day", to: "pages#day" 
+  get "week", to: "pages#week" 
+  get "intake", to: "pages#intake" 
+  get "practicefusiondefaultform", to: "pages#practicefusiondefaultform" 
+  get "intakenew", to: "pages#intakenew" 
+  get "task", to: "pages#task" 
+  get "mytask", to: "pages#mytask" 
+  get "tool", to: "pages#tool" 
+  get "unassignedtask", to: "pages#unassignedtask" 
+  get "reviewquestion", to: "pages#reviewquestion" 
+  get "rxchange", to: "pages#rxchange" 
+  get "labresults", to: "pages#labresults" 
+  get "refillrequests", to: "pages#refillrequests"
+  get "imagingresults", to: "pages#imagingresults"
+  get "chart", to: "pages#chart"
+  get "scheduled", to: "pages#scheduled"
+  get "recentpatients", to: "pages#recentpatients"
+  get "patientrecoad", to: "pages#patientrecoad"
+  get "timeline", to: "pages#timeline"
+  get "profile", to: "pages#profile"
+  get "recoaddate", to: "pages#recoaddate"
+  get "displaysetting", to: "pages#displaysetting"
+  get 'pages/appointment_modal', to: 'pages#appointment_modal'
+   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
